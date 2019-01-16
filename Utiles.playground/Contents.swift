281930e81a1fd62e9194642695c7a0bc09e34dfe
linder3hs <linderhassinger00@gmail.com>
2019-01-16 12:46:19 -0500

    Utiles Swift

---
+++ Utiles.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func hideKeyBoardWhenTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func makeAlert(title:String, message:String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func subString(text:String, start:Int) -> String {
+        let str = text
+        let indexText = str.index(str.startIndex, offsetBy: start)
+        let result = str[..<indexText]
+        return String(result)
+    }
+    
+}
+
